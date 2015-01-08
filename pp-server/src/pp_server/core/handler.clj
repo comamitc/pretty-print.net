c256d5cca031af0158a83c411b86d262d187cb5f
Mitch Comardo <mcomardo@pros.com>
2015-01-08 10:42:44 -0600

    Setting up the route for post formats to server

--- pp-server/src/pp_server/core/handler.clj
+++ pp-server/src/pp_server/core/handler.clj
@@ -3,8 +3,11 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+;; TODO: eventually abstract the route to take a type
+;; this will help use a map of functions to the type.
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (POST "/format/:type" request 
+    request) ;; TODO: just echos request currently
   (route/not-found "Not Found"))
 
 (def app
