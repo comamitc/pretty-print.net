8ee26e1f83ba594e456db7b097cb9316f7c5e5aa
Mitch Comardo <mcomardo@pros.com>
2015-01-08 16:15:06 -0600

    removing TODO's that are done or not needed.

--- pp-server/src/pp_server/core/handler.clj
+++ pp-server/src/pp_server/core/handler.clj
@@ -8,8 +8,6 @@
 (defn- decode-body! [req]
   (parse-string (slurp (:body req)) true))
 
-;; TODO: eventually abstract the route to take a type
-;; this will help use a map of functions to the type.
 (defroutes app-routes
   (POST "/format/:tipe" request
     (let [tipe (:tipe (:params request))
@@ -17,7 +15,6 @@
       (format-clj! (:input body) tipe)))
   (route/not-found "Not Found"))
 
-;; TODO: incorporate a configurable port number
 (def app (wrap-defaults app-routes
     ;; turn off anti-forgery crap for now
     (assoc-in site-defaults [:security :anti-forgery] false)))
