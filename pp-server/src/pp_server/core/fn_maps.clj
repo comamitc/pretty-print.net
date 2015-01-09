654c42aab8c5b5bbef5a4cc1a901604a73efa1e7
Mitch Comardo <mcomardo@pros.com>
2015-01-09 10:51:26 -0600

    xml pretty printing working; Added some TODOs.

--- pp-server/src/pp_server/core/fn_maps.clj
+++ pp-server/src/pp_server/core/fn_maps.clj
@@ -1,12 +1,17 @@
 (ns pp-server.core.fn-maps
   (:require [pp-server.format.clojure-pp :refer [format-clj!]]
-            [pp-server.format.web-pp :refer [format-json!]]))
+            [pp-server.format.web-pp :refer [format-json!]]
+            [pp-server.format.data-pp :refer [format-xml!]]))
 
+;; TODO: normalization of case for type
 (def typefn
   {"clojure"  format-clj!
    "edn"      format-clj!
-   "json"     format-json!})
+   "json"     format-json!
+   "xml"      format-xml!})
 
+;; TODO: try catch here in case the parser throws an error
+;; capture things like basic error and line number if possible
 (defn mapfn
   [input tipe]
   ((get typefn tipe) input tipe))
---
+++ pp-server/src/pp_server/format/data_pp.clj
@@ -0,0 +1,20 @@
+(ns pp-server.format.data-pp)
+
+;; http://nakkaya.com/2010/03/27/pretty-printing-xml-with-clojure/
+(defn format-xml!
+  "Takes a valid input xml input and pretty prints using standard java APIs"
+  [input tipe]
+  (let [in (javax.xml.transform.stream.StreamSource.
+            (java.io.StringReader. input))
+        writer (java.io.StringWriter.)
+        out (javax.xml.transform.stream.StreamResult. writer)
+        transformer (.newTransformer
+                     (javax.xml.transform.TransformerFactory/newInstance))]
+    (.setOutputProperty transformer
+                        javax.xml.transform.OutputKeys/INDENT "yes")
+    (.setOutputProperty transformer
+                        "{http://xml.apache.org/xslt}indent-amount" "2")
+    (.setOutputProperty transformer
+                        javax.xml.transform.OutputKeys/METHOD "xml")
+    (.transform transformer in out)
+    (-> out .getWriter .toString)))
