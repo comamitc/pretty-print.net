f381fe31b413e384b3aa07340382d564dfd9b039
Mitch Comardo <comamitc@gmail.com>
2015-01-13 19:09:14 -0600

    Fixed formatting issues with entire clojure and edn snippets

--- pp-jvm-server/src/pp_jvm/format/clojure_pp.clj
+++ pp-jvm-server/src/pp_jvm/format/clojure_pp.clj
@@ -7,36 +7,36 @@
   {"clojure"  code-dispatch
    "edn"      simple-dispatch})
 
+(defn- format-literal
+  "Takes a literal and formats it using the built in clojure.pprint/write
+  function. Returns a formatted literal."
+  [literal format-type]
+  (write literal
+         :pretty true
+         :stream nil
+         :dispatch (get dispatch-mode format-type)))
+
 (defn- read-all
   [input]
   (let [eof (Object.)]
     (take-while #(not= % eof) (repeatedly #(r/read input false eof)))))
 
-(defn str-to-literal!
+(defn str-to-literal
   "Takes valid clojure as input and transforms it to a clojure literal for
   formating."
-  [string]
+  [string format-type]
   (read-all (rt/string-push-back-reader string)))
 
-(defn- format-literal!
-  "Takes a literal and formats it using the built in clojure.pprint/write
-  function. Returns a formatted literal."
-  [literal tipe]
-  (write literal
-         :pretty true
-         :stream nil
-         :dispatch (get dispatch-mode tipe)))
-
-(defn- literal-to-str!
+(defn- literal-to-str
   "Takes valid formatter clojure literal as input and transforms it to a string
   for return to webapp UI"
   [literal]
-  (str literal))
+   (clojure.string/join "\n\n" literal))
 
-(defn format-clj!
+(defn format-clj
   "Clojure formatting function that takes a unformatted-input and format type
   and transforms it into a formatted output using clojure.pprint/write."
-  [input tipe]
-  (let [in-lit    (str-to-literal! input)
-        formatted (format-literal! in-lit tipe)]
-        (literal-to-str! formatted)))
+  [input format-type]
+  (let [in-lits  (str-to-literal input format-type)
+       formatted (map #(format-literal % format-type) in-lits)]
+        (literal-to-str formatted)))
