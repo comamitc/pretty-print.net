b65f723a0564efe1dbd42daa408b8df55d11e562
Mitch Comardo <mcomardo@pros.com>
2015-01-15 16:00:50 -0600

    uploaded clj-beautify to clojars and downloaded as a packaged dep.

--- pp-jvm-server/project.clj
+++ pp-jvm-server/project.clj
@@ -3,6 +3,7 @@
   :url "http://pretty-print.net"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-beautify "0.1.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
                  [cheshire "5.3.1"]
@@ -12,10 +13,7 @@
                                                     javax.jms/jms
                                                     com.sun.jmdk/jmxtools
                                                     com.sun.jmx/jmxri]]]
-  :plugins [[lein-ring "0.8.13"]
-            [lein-git-deps "0.0.2-SNAPSHOT"]]
-  :git-dependencies [["https://github.com/comamitc/clj-beautify.git"]]
-  :aot [clj-beautify.core]
+  :plugins [[lein-ring "0.8.13"]]
   :ring {:handler pp-jvm.core.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
