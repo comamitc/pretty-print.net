4e26b5f0c1a0609d468ff7cc120a649920e32b5c
Chris Oakman <chris@oakmac.com>
2015-01-18 16:08:42 -0600

    Houston, we have a header.

--- frontend/src-cljs/pp_client/html.cljs
+++ frontend/src-cljs/pp_client/html.cljs
@@ -4,6 +4,21 @@
     hiccups.runtime
     [pp-client.util :refer [js-log log]]))
 
+;;------------------------------------------------------------------------------
+;; HTML is super cool
+;;------------------------------------------------------------------------------
+
+(hiccups/defhtml header []
+  [:header.outer-c5e7d
+    [:div.inner-0f889
+      [:div.left-9467a
+        [:a.home-link-e4c1e {:href ""}
+          "pretty-print"
+          [:span.net-84e9a ".net"]]]
+      [:div.right-1dd94 "right"]
+      [:div.clr-217e3]]])
+
 (hiccups/defhtml page []
+  (header)
   [:div "I can quit anytime."]
   )
