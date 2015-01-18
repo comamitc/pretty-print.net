93b1ed685b760d337d74b05faba839c892e6c90d
Mitch Comardo <comamitc@gmail.com>
2015-01-18 13:06:07 -0600

    added cljs-client via build tool; going to write client in cljs

---
+++ 00-scrap/client-ideas.md
@@ -0,0 +1,9 @@
+- raw mode
+
+- save formats for up to a week in text files
+
+- single page
+
+- ability to bookmark and save your settings (type of formatting)
+
+- format settings based on formatter
---
+++ pp-cljs-client/pp_cljs_client.js
@@ -0,0 +1,8 @@
+goog.addDependency("base.js", ['goog'], []);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
+goog.addDependency("../pp_cljs_client/util.js", ['pp_cljs_client.util'], ['cljs.core']);
+goog.addDependency("../pp_cljs_client/dom.js", ['pp_cljs_client.dom'], ['goog.dom', 'pp_cljs_client.util', 'cljs.core']);
+goog.addDependency("../clojure/browser/event.js", ['clojure.browser.event'], ['cljs.core', 'goog.events.EventTarget', 'goog.events.EventType', 'goog.events']);
+goog.addDependency("../clojure/browser/net.js", ['clojure.browser.net'], ['goog.net.xpc.CfgFields', 'goog.net.XhrIo', 'goog.json', 'goog.Uri', 'cljs.core', 'goog.net.EventType', 'goog.net.xpc.CrossPageChannel', 'clojure.browser.event']);
+goog.addDependency("../clojure/browser/repl.js", ['clojure.browser.repl'], ['cljs.core', 'clojure.browser.net', 'clojure.browser.event']);
+goog.addDependency("../pp_cljs_client/core.js", ['pp_cljs_client.core'], ['clojure.browser.repl', 'cljs.core']);
