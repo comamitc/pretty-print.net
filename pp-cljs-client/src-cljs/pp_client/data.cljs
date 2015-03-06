b2cfe0166a153f6ab1ed2704eb3b4a67aa8263fd
Mitch Comardo <comamitc@gmail.com>
2015-03-05 20:55:26 -0600

    settings on client is empty for now until we have a UI

--- pp-cljs-client/src-cljs/pp_client/data.cljs
+++ pp-cljs-client/src-cljs/pp_client/data.cljs
@@ -4,7 +4,7 @@
     [pp-client.util :refer [js-log log]]))
 
 (defn format-input [url payload handler error-handler]
-  (POST url {:params {:input payload}
+  (POST url {:params {:input payload :settings {}}
              :format :json
              :keywords? true
              :response-format :json
