b1fa47ed8ec7339f8e8755bb34325bcf35ae8697
Mitch Comardo <mcomardo@cmn.com>
2016-01-22 15:47:56 -0600

    Fix parsing exception

--- pp-cljs-client/project.clj
+++ pp-cljs-client/project.clj
@@ -5,7 +5,7 @@
 
   :dependencies [
     [org.clojure/clojure "1.6.0"]
-    [org.clojure/clojurescript "0.0-2913"]
+    [org.clojure/clojurescript "0.0-2985"]
     [cljsjs/react "0.12.2-7"]
     [cljs-ajax "0.3.10"]
     [sablono "0.3.4"]
@@ -16,7 +16,7 @@
 
   :source-paths ["src"]
 
-  :clean-targets ["target" "public/out" "public/js/pretty-print.js" "public/js/pretty-print.min.js"]
+  :clean-targets ["target" "public/out" "public/out-min" "public/js/pretty-print.js" "public/js/pretty-print.min.js"]
 
   :cljsbuild {
     :builds {
@@ -31,5 +31,6 @@
         :source-paths ["src-cljs"]
         :compiler {
           :output-to "public/js/pretty-print.min.js"
+          :output-dir "public/out-min"
           :optimizations :advanced
           :pretty-print false }}}})
