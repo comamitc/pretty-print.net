5fb033554f680c96108340b55382c910fef520c0
Mitch Comardo <comamitc@gmail.com>
2016-01-22 23:45:22 -0600

    adjusting run command

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 echo "starting new server build..."
 cd ./pp-jvm-server
-nohup java -jar -Xmx384m target/pp-jvm-server-0.1.0-standalone.jar &
+
+nohup java -jar -Xmx384m target/pp-jvm-server-0.1.0-standalone.jar > nohup.out 2>&1 &
