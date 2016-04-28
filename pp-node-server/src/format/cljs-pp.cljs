539f2c0c07a705eb217f485472bd3d12df58659e
Mitch Comardo <comamitc@gmail.com>
2016-04-28 09:30:16 -0500

    initial implementation of CLJS pprint

--- pp-node-server/src/format/cljs-pp.cljs
+++ pp-node-server/src/format/cljs-pp.cljs
@@ -1,6 +1,44 @@
 (ns format.clj-pp
-  (:require [cljs.nodejs :as node]
-            [utils :as util]))
+  (:require [cljs.pprint :refer [write
+                                 *print-pretty*
+                                 *print-pprint-dispatch*
+                                 *print-right-margin*
+                                 *print-miser-width*
+                                 *print-base*
+                                 *print-radix*
+                                 *print-suppress-namespaces*
+                                 code-dispatch
+                                 simple-dispatch]]
+            [cljs.reader :as reader]))
 
-(defn format-clj [input _]
-  input)
+(def dispatch-mode {"clj" code-dispatch
+                    "edn" simple-dispatch})
+
+(def default-settings {:right-margin        *print-right-margin*
+                       :miser-width         *print-miser-width*
+                       :base                *print-base*
+                       :level               *print-level*
+                       :radix               *print-radix*
+                       :suppress-namespaces *print-suppress-namespaces*
+                       :pretty              *print-pretty*
+                       :dispatch            *print-pprint-dispatch*})
+
+(defn format-string
+  [input mode settings]
+  (let [presets (merge default-settings settings)
+        result  (write (reader/read-string input)
+                       :pretty              (:pretty presets)
+                       :stream              nil
+                       :right-margin        (:right-margin presets)
+                       :miser-width         (:miser-width presets)
+                       :base                (:base presets)
+                       :length              (:length presets)
+                       :radix               (:radix presets)
+                       :suppress-namespace  (:suppress-namespace presets)
+                       :dispatch            (get dispatch-mode format-type))]
+    (print result)
+    result))
+
+(defn format-clj [tipe input settings]
+  ([input mode] (format-clj input mode nil))
+  ([input mode settings] (format-string input mode settings)))
