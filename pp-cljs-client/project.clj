2f55b543bc25e6faa7540b8603b526dca05c33ef
Mitch Comardo <comamitc@gmail.com>
2016-04-28 09:29:17 -0500

    formatting

--- pp-cljs-client/project.clj
+++ pp-cljs-client/project.clj
@@ -4,13 +4,13 @@
   :url "http://example.com/FIXME"
 
   :dependencies [
-    [org.clojure/clojure "1.6.0"]
-    [org.clojure/clojurescript "0.0-2985"]
-    [cljsjs/react "0.12.2-7"]
-    [cljs-ajax "0.3.10"]
-    [sablono "0.3.4"]
-    [secretary "1.2.1"]
-    [quiescent "0.1.4"]]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2985"]
+                 [cljsjs/react "0.12.2-7"]
+                 [cljs-ajax "0.3.10"]
+                 [sablono "0.3.4"]
+                 [secretary "1.2.1"]
+                 [quiescent "0.1.4"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]]
 
@@ -21,18 +21,18 @@
   :jvm-opts ["-Xmx384m"]
 
   :cljsbuild {
-    :builds {
-      :main {
-        :source-paths ["src-cljs"]
-        :compiler {
-          :output-to "public/js/pretty-print.js"
-          :output-dir "public/out"
-          :optimizations :whitespace}}
+              :builds {
+                       :main {
+                              :source-paths ["src-cljs"]
+                              :compiler {
+                                         :output-to "public/js/pretty-print.js"
+                                         :output-dir "public/out"
+                                         :optimizations :whitespace}}
 
-      :main-min {
-        :source-paths ["src-cljs"]
-        :compiler {
-          :output-to "public/js/pretty-print.min.js"
-          :output-dir "public/out-min"
-          :optimizations :advanced
-          :pretty-print false }}}})
+                       :main-min {
+                                  :source-paths ["src-cljs"]
+                                  :compiler {
+                                             :output-to "public/js/pretty-print.min.js"
+                                             :output-dir "public/out-min"
+                                             :optimizations :advanced
+                                             :pretty-print false}}}})
