19a97286c16f5466498dab7b66b97a31b1b1e8d5
Mitch Comardo <comamitc@gmail.com>
2016-08-04 15:42:17 -0500

    one more build refinement

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-echo "stopping current server..."
-kill `lsof -n -iTCP:5000 -sTCP:LISTEN -t`
-
 # copy default config file
 echo "copying config file..."
 cp ./config/config.example.json ./config/config.json
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-echo "starting new server build..."
+echo "stopping current server..."
+kill `lsof -n -iTCP:7000 -sTCP:LISTEN -t`
 
+echo "starting new server build..."
 nohup node target/server/index.js > output.log &
