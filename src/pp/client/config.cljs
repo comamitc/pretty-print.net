4b8932a43c8801776ce16f4033e0ee77a45e976c
Mitch Comardo <comamitc@gmail.com>
2016-08-04 16:55:47 -0500

    removing config section

--- src/pp/client/config.cljs
+++ src/pp/client/config.cljs
@@ -1,46 +1,15 @@
 (ns pp.client.config)
 
-(def default-clj-settings {:right-margin
-                              {:type "range"
-                               :id "right-margin"
-                               :name "Right Margin:"
-                               :class "range-input-0b991"
-                               :min 20
-                               :max 120
-                               :step 5
-                               :value 80}
-                           :miser-width
-                              {:type "range"
-                               :id "miser-width"
-                               :name "Miser Width:"
-                               :class "range-input-0b991"
-                               :min 20
-                               :max 120
-                               :step 5
-                               :value 40}
-                            :pretty
-                              {:type "checkbox"
-                               :id "pretty"
-                               :name "Pretty?"
-                               :class "cmn-toggle cmn-toggle-round-flat"
-                               :checked true}
-                            :suppress-namespaces
-                              {:type "checkbox"
-                               :id "suppress-namespaces"
-                               :name "Suppress Namespace?"
-                               :class "cmn-toggle cmn-toggle-round-flat"
-                               :checked false}})
-
 (def style-map {"edn"
                   {:id "edn"
                    :desc "EDN"
                    :uri "/node/format/edn"
-                   :settings default-clj-settings}
+                   :settings {}}
                 "clj"
                   {:id "clj"
                    :desc "Clojure Code"
                    :uri "/node/format/clj"
-                   :settings default-clj-settings}
+                   :settings {}}
                 "json"
                   {:id "json"
                    :desc "JSON"
