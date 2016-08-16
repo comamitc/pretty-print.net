a25a1bce99092383488a2b89d333adefed06d8d6
Mitch Comardo <comamitc@gmail.com>
2016-08-15 21:50:52 -0500

    merge some stupid stuff
    # Conflicts:
    #	src/pp/client/html.cljs
    #	src/pp/client/routes.cljs

--- src/pp/client/html.cljs
+++ src/pp/client/html.cljs
@@ -1,6 +1,5 @@
 (ns pp.client.html
   (:require [rum.core :as rum]
-<<<<<<< HEAD
             [clojure.string :as s]
             [pp.client.util :refer [js-log log]]
             [pp.client.dom :refer [by-id]]
@@ -33,20 +32,4 @@
 
 (defn main-page []
   (rum/mount (page-contents) (.getElementById js/document "bodyWrapper")))
-=======
-            [pp.client.util :refer [js-log log]]
-            [pp.client.dom :refer [by-id]]
-            [pp.client.config :refer [state]]))
-
-(log state)
-
-(rum/defc page-contents []
-  [:div#pageWrapper
-    [:div.body-outer-7cb5e
-      [:div.body-inner-40af1
-        [:h2.instructions-b15d3 (str "Paste " (:desc state) ":")]
-        [:div.clr-217e3]]]])
-
-(defn main-page [style]
-  (rum/mount (page-contents) (by-id "bodyWrapper")))
->>>>>>> origin/44-client-format
+>>>>>>> setup the start of RUM
