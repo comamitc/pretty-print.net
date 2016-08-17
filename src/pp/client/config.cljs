3721d323667a7761fe7dfeda5e07538cd3cef7a8
Mitch Comardo <comamitc@users.noreply.github.com>
2016-08-17 10:18:17 -0500

    [RFC] Long EDN not wrapping? (#45)
    Long EDN not wrapping?

--- src/pp/client/config.cljs
+++ src/pp/client/config.cljs
@@ -1,7 +1,8 @@
 (ns pp.client.config)
 
 (def style-map
-  {:clj "Clojure"
+  {:clj  "Clojure"
+   :edn  "EDN"
    :json "JavaScript"})
 
-(def state (atom {:success? false :error? false :value nil :style :clj :cm nil}))
+(def state (atom {:success? false :error? false :value nil :style :edn :cm nil}))
--- src/pp/client/format.cljs
+++ src/pp/client/format.cljs
@@ -1,6 +1,8 @@
 (ns pp.client.format
   (:require [cljfmt.core :refer [reformat-string]]
             [pp.client.util :refer [js-log log]]
+            [cljs.pprint :as p]
+            [cljs.reader :as r]
             cljsjs.js-beautify
             cljsjs.parinfer))
 
@@ -12,13 +14,28 @@
       (assoc {} :error e))))
 
 ;; compose a function to transfor erroneous output
-(def format-clj (comp reformat-string
+(def  format-clj (comp reformat-string
+                       #(.-text %)
+                       js/parinfer.indentMode))
+
+(defn- pp-str [s]
+  (-> s
+      r/read-string
+      (p/write :right-margin        80
+               :stream              nil
+               :miser-width         120
+               :dispatch            p/simple-dispatch)))
+
+
+(def format-edn (comp reformat-string
+                      pp-str
                       #(.-text %)
                       js/parinfer.indentMode))
 
 (defn format-input! [style input]
   (case style
     :clj  (normalize-result format-clj input)
+    :edn  (normalize-result format-edn input)
     :json (normalize-result js/js_beautify input {:indent_size           2
                                                   :indent_char           " "
                                                   :max_preserve_newlines 2})))
